7c6215c01c7e6d167e01eb6183b3b4ddafbd27d3
Drew Krehel <drewkrehel@gmail.com>
2025-04-15 00:07:24 +0000

    still setting up

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "sinatra-activerecord"
 
 gem "http"
 gem "dotenv"
+gem "ruby-openai"
 
 group :development do
   gem "better_errors"
--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
 require "better_errors"
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
 
 get("/") do
   erb(:home)
@@ -12,3 +16,36 @@ get("/translation") do
 
   erb(:translation)
 end
+
+# OpenAI Resource 
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+#pp api_response
+parsed_choice = api_response.fetch("choices")
+choice_result = parsed_choice.at(0)
+parsed_message = choice_result.fetch("message")
+first_message = parsed_message.fetch("content")
+
+#Program Start
+puts "Hello! How can I help you today?"
+puts 50*"-"
+user_input = gets.chomp
+message_list << @trans_input
